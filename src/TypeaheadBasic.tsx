fbe5325c346f1d6da2eb72fd6cc213cba1c60b3c
Sebastian Perez <sebasplashba@gmail.com>
2023-10-24 11:55:56 -0600

    Readme

--- src/TypeaheadBasic.tsx
+++ src/TypeaheadBasic.tsx
@@ -1,20 +1,21 @@
-import { Typeahead } from "react-bootstrap-typeahead";
-import "react-bootstrap-typeahead/css/Typeahead.css";
-import { Get } from "./api/Get";
-import { useState } from "react";
+import { Typeahead } from 'react-bootstrap-typeahead';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
+import { getData } from './api/get';
+import { useState } from 'react';
 
 export const TypeaheadBasic = () => {
   const [optionsCityState, setOptionsCityState] = useState([]);
   const [optionZipCode, setOptionZipCode] = useState([]);
 
   const fetchData = async (value: string) => {
-    const { cityState, zipCode } = await Get(value);
+    const { cityState, zipCode } = await getData(value);
+
     setOptionsCityState(cityState);
     setOptionZipCode(zipCode);
   };
 
   const onInputChangeValue = (input: string) => {
-    if (input !== "") {
+    if (input !== '') {
       fetchData(input);
     }
   };
@@ -23,11 +24,10 @@ export const TypeaheadBasic = () => {
     <>
       <Typeahead
         onInputChange={onInputChangeValue}
-        id="typeahead-basic"
+        id='typeahead-basic'
         minLength={1}
         options={[...optionZipCode, ...optionsCityState]}
-        className="m-4 w-25"
-        paginate={false}
+        className='m-4 w-25'
       />
     </>
   );
--- src/api/Get.ts
+++ src/api/Get.ts
@@ -1,27 +1,12 @@
-
-
-export const Get = async (value: string) => {
-
-  const res = await fetch(`https://typeahead-sebas-api.onrender.com/data/${value}`)
-  const data = await res.json()
-  const zipCode = data.map((zip: any) => (zip.zip_code.toString()))
-  const cityState = data.map((cs: any) => (`${cs.city}, ${cs.state}`))
+export const getData = async (value: string) => {
+  const res = await fetch(
+    `https://typeahead-sebas-api.onrender.com/data/${value}`
+  );
+  const data = await res.json();
+  const zipCode = data.map((zip: any) => zip.zip_code.toString());
+  const cityState = data.map((cs: any) => `${cs.city}, ${cs.state}`);
   return {
     cityState,
-    zipCode
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const res = await fetch(`https://typeahead-pi.vercel.app/api/findPlace/obtainInfo?searchTerm=new`)
+    zipCode,
+  };
+};
